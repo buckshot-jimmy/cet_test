28a947ab90a55a15f3de3df2fab50cff759aaa1f
Ciprian Marta <ciprianmarta.cm@gmail.com>
2026-05-25 11:13:08 +0300

    cont

--- src/PDF/DTO/PdfDocument.php
+++ src/PDF/DTO/PdfDocument.php
@@ -4,5 +4,9 @@ namespace App\PDF\DTO;
 
 class PdfDocument
 {
-    public function __construct(public array $data, public array $dateFirma, public string $outputName ) {}
+    public function __construct(
+        public array $data,
+        public array $dateFirma,
+        public string $outputName )
+    {}
 }
